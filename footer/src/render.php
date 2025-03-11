34c3cc7eb5a8c499283e358d0ad9e2f064aea807
amunpote <amun.pote@dataover.io>
2025-03-11 10:43:57 +0545

    fix: remove render file

--- footer/src/render.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client_id = isset( $attributes['clientId'] ) ? str_replace( '-', '_', sanitize_key( wp_unslash( $attributes['clientId'] ) ) ) : '';
-
-$block_id = 'cthf_' . $client_id;
-
-echo $content;
