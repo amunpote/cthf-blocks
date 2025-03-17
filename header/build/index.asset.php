ca5c8aa069bc613e0d22775801b9c3b43ec92fd1
amunpote <amun.pote@dataover.io>
2025-03-17 11:05:45 +0545

    add: pro option for top bar sticky

--- header/build/index.asset.php
+++ header/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '57d8dd365b11b65622ed');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '709e16d825848d1308c5');
