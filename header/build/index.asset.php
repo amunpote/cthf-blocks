69b22ad830ee30034dc7dd1b83850453ba047c5d
amunpote <amun.pote@dataover.io>
2025-05-27 11:35:23 +0545

    fix: search modal header dynamic text

--- header/build/index.asset.php
+++ header/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '71efb505988396b8f069');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '50724589e13326724555');
