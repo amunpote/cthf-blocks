2b0f0c14c8f17b574da197bbed8cdc98e4560929
amunpote <amun.pote@dataover.io>
2025-05-30 19:16:11 +0545

    fix: project structure

--- copyright-text/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eb6bfb5856e1a8ab5299');
